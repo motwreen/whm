8a770263578d4579b9ae60386a9550f114a30f85
Ahmed Gad <admin@motwreen.com>
2018-01-07 02:47:48 +0200

    Fix Defult Port

--- src/config/whm.php
+++ src/config/whm.php
@@ -1,7 +1,7 @@
 <?php
     return[
         'host'     => 'SERVER',        //Server Domain
-        'port'     => '2078',          //Server PORT Default : 2078
+        'port'     => '2087',          //Server PORT Default : 2087
         'endpoint' => 'json-api',      //API Type
         'username' => 'root',          //Server Root Username
         'password' => 'ROOT PASSWORD', //server root password
